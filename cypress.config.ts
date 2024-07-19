49c6e9a6050fa6f5093a9bf3063a1cd6426bb703
Tianrui Zhang (Gary) <tianrui.zhang.2101@stu.moonshotacademy.cn>
2024-07-19 14:57:15 +0800

    test(cypress): 增加覆盖性测试

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
     },
   },
 
   component: {
     devServer: {
-      framework: "next",
-      bundler: "webpack",
+      framework: 'next',
+      bundler: 'webpack',
     },
   },
 });
