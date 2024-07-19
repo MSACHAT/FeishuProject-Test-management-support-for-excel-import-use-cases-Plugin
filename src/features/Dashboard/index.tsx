097438ad82eddda9223b9bfbe49c7f16b1f6809f
StanH123h <1735443634@qq.com>
2024-07-19 14:11:55 +0800

    feat (Dashboard): 完成了excel导入和基本的解析

--- src/features/Dashboard/index.tsx
+++ src/features/Dashboard/index.tsx
@@ -1,16 +1,114 @@
 import { hot } from 'react-hot-loader/root';
 import React, { useContext, useEffect, useRef, useState } from 'react';
-import { Upload, Button, Modal, Steps } from '@douyinfe/semi-ui';
-import { IconUpload } from '@douyinfe/semi-icons';
+import { Upload, Button, Modal, Steps, ToastFactory, Card, Avatar } from '@douyinfe/semi-ui';
 import './index.less';
+import { IconFile, IconHelpCircle } from '@douyinfe/semi-icons';
 import * as XLSX from 'xlsx';
+import Meta from '@douyinfe/semi-ui/lib/es/card/meta';
+import { BeforeUploadProps } from '@douyinfe/semi-ui/lib/es/upload';
 
 const STEP_1_UPLOAD = 0;
 const STEP_2_PREVIEW = 1;
 const STEP_3_FINISH = 2;
 
+const SetIsReadyForNextStepContext = React.createContext((isReady: boolean) => {
+});
+const SetCurrentErrorContext = React.createContext((currentErr: string) => {
+});
 const SetFileNameContext = React.createContext((filename: string) => {
 });
+const SetFileContext = React.createContext((file:File)=>{})
+
+//创建一个在页面顶部且置顶的弹窗
+const ToastOnTop = ToastFactory.create({
+  zIndex: 99999,
+});
+
+/*
+   此处封装了每一步独特的页面的组件
+
+   参数:无
+
+   返回值:progress组件
+    */
+const StepContent = ({ currentStep, parsedExcelData, setParsedExcelData }) => {
+  const [fileName, setFileName] = useState<string>('');
+  const [file, setFile] = useState<File>();
+
+  /*
+  此处封装了用于检查表格是否存在错误的函数
+
+  参数:
+    1. worksheet:类型未XLSX.Worksheet的参数，也就是一个表格
+
+  返回值:
+    1. 没有错误: {hasError:false, errMessage:""}
+    2. 有错误: {hasError:true, errMessage:<错误信息>
+   */
+  const checkErr=(worksheet:XLSX.WorkSheet)=>{
+
+  }
+
+  switch (currentStep) {
+    case STEP_1_UPLOAD:
+      return <>
+        <SetFileNameContext.Provider value={(filename: string) => setFileName(filename)}>
+          <SetFileContext.Provider value={(file) => setFile(file)}>
+            <UploadComponent callBack={setParsedExcelData} />
+          </SetFileContext.Provider>
+        </SetFileNameContext.Provider>
+        <div className={'dashboard-container-file-display'}>
+          <Card
+            style={{ maxWidth: '50%' }}
+            bodyStyle={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Meta
+              title={fileName ? fileName : '未上传文件'}
+              avatar={
+                <Avatar size="default" style={{ margin: 4 }}>
+                  {fileName ? <IconFile /> : <IconHelpCircle />}
+                </Avatar>
+              }
+            />
+          </Card>
+        </div>
+      </>;
+    case STEP_2_PREVIEW:
+      const reader = new FileReader();
+      reader.onload = (e) => {//当文件读取完成时onload就会被触发
+        if (e.target && e.target.result) {
+          const data = new Uint8Array(e.target.result as ArrayBuffer);
+          const workbook = XLSX.read(data, { type: 'array' });
+          const sheetName = workbook.SheetNames[0];
+          const worksheet = workbook.Sheets[sheetName];
+          const jsonData = XLSX.utils.sheet_to_json(worksheet);
+          setParsedExcelData(jsonData)
+        } else {
+          throw Error('数据解析时遇到错误!');
+        }
+      };
+      if(file) {
+        reader.readAsArrayBuffer(file);
+      }
+
+      return <div>
+        {parsedExcelData.map((data) => {
+          let newData: string[] = [];
+          for (let dat of Object.keys(data)) {
+            newData.push(`${dat}:${data[dat]} \n`);
+          }
+          return newData;
+        })}
+      </div>;
+    case STEP_3_FINISH:
+      return <div>wancheng</div>;
+  }
+  return <UploadComponent callBack={setParsedExcelData} />;
+};
 
 /*
   此处封装了弹窗中每一步对应的页面
@@ -24,48 +122,15 @@ const SetFileNameContext = React.createContext((filename: string) => {
    */
 const ProgressComponent = ({ currentStep }: { currentStep: number }) => {
   const [parsedExcelData, setParsedExcelData] = useState<any>([]);
-  const [fileName, setFileName] = useState<string>('还未选择文件');
-
-  /*
-   此处封装了每一步独特的页面的组件
-
-   参数:无
-
-   返回值:progress组件
-    */
-  const StepContent = () => {
-    switch (currentStep) {
-      case STEP_1_UPLOAD:
-        return <>
-          <SetFileNameContext.Provider value={(filename: string) => setFileName(filename)}>
-            <UploadComponent callBack={setParsedExcelData} />
-          </SetFileNameContext.Provider>
-          <br />
-          {fileName}
-        </>;
-      case STEP_2_PREVIEW:
-        return <div>
-          {parsedExcelData.map((data) => {
-            let newData: string[] = [];
-            for (let dat of Object.keys(data)) {
-              newData.push(`${dat}:${data[dat]} \n`);
-            }
-            return newData;
-          })}
-        </div>;
-      case STEP_3_FINISH:
-        return <div>wancheng</div>;
-    }
-    return <UploadComponent callBack={setParsedExcelData} />;
-  };
   return (
     <>
       <Steps type="basic" current={currentStep} onChange={(i) => console.log(i)}>
-        <Steps.Step title="第三步:完成" description="大功告成" />
-        <Steps.Step title="第二步:预览" description="预览导入的结果" />
         <Steps.Step title="第一步:导入" description="导入xlsx. csv. 格式的文件" />
+        <Steps.Step title="第二步:预览" description="预览导入的结果" />
+        <Steps.Step title="第三步:完成" description="大功告成" />
       </Steps>
-      <StepContent />
+      <StepContent currentStep={currentStep} parsedExcelData={parsedExcelData}
+                   setParsedExcelData={setParsedExcelData} />
     </>
   )
     ;
@@ -80,7 +145,9 @@ const ProgressComponent = ({ currentStep }: { currentStep: number }) => {
   返回值:UploadComponent组件
   */
 const UploadComponent = ({ callBack }) => {
+  const setIsReadyForNextStep = useContext(SetIsReadyForNextStepContext);
   const setFileName = useContext(SetFileNameContext);
+  const setFile = useContext(SetFileContext)
 
   /*
    此函数会在用户上传完文件后执行，作用是对文件进行解析
@@ -90,45 +157,30 @@ const UploadComponent = ({ callBack }) => {
 
    返回值:false,由于没有后端所以直接返回false防止Upload组件自动上传
     */
-  const handleBeforeUpload = (file) => {
+  const handleBeforeUpload = (file:BeforeUploadProps):boolean => {
     const blob = file.file.fileInstance;
-    setFileName(blob.name);
-    const reader = new FileReader();
-    reader.onload = (e) => {//当文件读取完成时onload就会被触发
-      if (e.target && e.target.result) {
-        const data = new Uint8Array(e.target.result as ArrayBuffer);
-        const workbook = XLSX.read(data, { type: 'array' });
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-        const jsonData = XLSX.utils.sheet_to_json(worksheet);
-        callBack(jsonData);
-      } else {
-        throw Error('error during data resolving!');
-      }
-    };
-    reader.readAsArrayBuffer(blob);
-    return true;
-  };
+    if(blob) {
+      setFile(blob)
+      setFileName(blob.name);
+      setIsReadyForNextStep(true);
+    }
+    else{
+      ToastOnTop.error("文件已损坏");
+    }
 
-  const UploadButton = (
-    <Button icon={<IconUpload />} theme="light">
-      点击上传
-    </Button>
-  );
+    return false;
+  };
 
   return (
     <>
-      <div
-        style={{
-          marginBottom: 12,
-          marginTop: 12,
-          borderBottom: '1px solid var(--semi-color-border)',
-        }}
-      ></div>
       <Upload
+        draggable={true}
+        dragMainText={'点击上传文件或拖拽文件到这里'}
+        dragSubText="仅支持.xlsx, .csv格式的文件"
+        accept={'.xlsx, .csv'}
+        fileList={[]}//防止Update组件渲染filelist
         beforeUpload={handleBeforeUpload}
       >
-        {UploadButton}
       </Upload>
     </>
   );
@@ -138,37 +190,56 @@ export default hot(() => {
   const [visible, setVisible] = useState(false);
   const [currentStep, setCurrentStep] = useState(0);
   const currentStepRef = useRef(0);
+  const [isReadyForNextStep, setIsReadyForNextStep] = useState(false);
+  //此变量用于保存当前无法进行下一步操作的原因
+  const [currentError, setCurrentError] = useState('未上传文件');
   useEffect(() => {
     setCurrentStep(currentStepRef.current);
   }, [currentStep]);
-  const showDialog = () => {//显示弹窗
+
+  //显示弹窗
+  const showDialog = () => {
     setCurrentStep(0);
     currentStepRef.current = 0;
     setVisible(true);
   };
+
   const handleOk = () => {
+    if (!isReadyForNextStep) {
+
+      ToastOnTop.error(currentError);
+      return;
+    }
     setCurrentStep(currentStep + 1);
+    setIsReadyForNextStep(false);
     currentStepRef.current += 1;
     console.log('Ok button clicked');
   };
-  const handleCancel = () => {//关闭弹窗
+
+  //关闭弹窗
+  const handleCancel = () => {
     setVisible(false);
     console.log('Cancel button clicked');
   };
+
   return (
-    <div className="dashboard-container">
+    <div className="dashboard-container" id="dashboard-container">
       <Button onClick={showDialog}>打开弹窗</Button>
       <Modal
         title="基本对话框"
         visible={visible}
         onOk={handleOk}
         onCancel={handleCancel}
-        closeOnEsc={true}
+        closeOnEsc={false}
         width={'80vw'}
         height={'80vh'}
         okText={'下一步'}
       >
-        <ProgressComponent currentStep={(currentStepRef.current)} />
+        <SetIsReadyForNextStepContext.Provider value={(isReady) => setIsReadyForNextStep(isReady)}>
+          <SetCurrentErrorContext.Provider value={(currentErr) => setCurrentError(currentErr)}>
+            <ProgressComponent currentStep={(currentStepRef.current)} />
+          </SetCurrentErrorContext.Provider>
+        </SetIsReadyForNextStepContext.Provider>
       </Modal>
     </div>
   );
